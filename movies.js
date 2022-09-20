5a6544c785bedf7c16f70eb80b2b8bb289ee5efb
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-09-20 15:50:53 -0500

    fetch request details add

--- movies.js
+++ movies.js
@@ -1,1 +1,12 @@
 "use strict";
+// URL for Glitch fake Movies API
+const urlGlitch = 'https://hissing-acute-crafter.glitch.me/movies'
+
+// Example of creating an options object - best practice may be to make one for each type of request we are attempting.
+const options = {
+    method: 'POST', // this changes to get post or get or put or patch
+    headers: {
+        'Content-Type': 'application/json', // leave this header alone for all the options objects we make
+    },
+    body: JSON.stringify(blogPost), // blogPost will change to include details about what we are stringifying
+};
