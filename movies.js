d476c54dbf19b260c74108ea0b8d863b1d135cc6
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-09-21 11:45:47 -0500

    we got datagit add . git add .

--- movies.js
+++ movies.js
@@ -1,12 +1,24 @@
 "use strict";
+
 // URL for Glitch fake Movies API
 const urlGlitch = 'https://hissing-acute-crafter.glitch.me/movies'
 
 // Example of creating an options object - best practice may be to make one for each type of request we are attempting.
-const options = {
-    method: 'POST', // this changes to get post or get or put or patch
-    headers: {
-        'Content-Type': 'application/json', // leave this header alone for all the options objects we make
-    },
-    body: JSON.stringify(blogPost), // blogPost will change to include details about what we are stringifying
+// const options = {
+//     method: 'POST', // this changes to get post or get or put or patch
+//     headers: {
+//         'Content-Type': 'application/json', // leave this header alone for all the options objects we make
+//     },
+//     body: JSON.stringify(blogPost), // blogPost will change to include details about what we are stringifying
+// };
+
+const getMovieData = async () => {
+    try {
+        const res = await axios.get(urlGlitch);
+        console.log(res.data);
+    } catch (e) {
+        console.log("ERROR", e);
+    }
 };
+
+getMovieData();
