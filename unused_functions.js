3c3834a2a9985054a69ddf36a67f7f5600a6b771
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-10-04 05:58:44 -0500

    clean up js file

--- unused_functions.js
+++ unused_functions.js
@@ -1,32 +1,5 @@
 // WORKING FUNCTIONS NOT NEEDED IN THE PROJECT AT THE MOMENT
 
-// GET MOVIE INFO by SPECIFIC ID
-    // Works only with hard coded url with specified query string
-const getMovieInfo = async () => {
-    try {
-        const res = await axios.get(`https://hissing-acute-crafter.glitch.me/movies/?id=1`);
-        return res.data[0];
-    } catch (e) {
-        return `It's broken, fix it! ${e}`;
-    }
-
-};
-
-// ADD ALL MOVIE TILES ON PAGE
-    // This works and is currently in use, however saving working copy here incase I break it while trying to fix it!
-const allMoviesAdded = async () => {
-    const allMovieData = await getMoviesData()
-    for (let movie of allMovieData) {
-        const createTile = document.createElement("div")
-        createTile.setAttribute("class", "card movie-tile");
-        createTile.setHTML
-        (`<p>${movie.title}</p>
-            <p>${movie.rating}</p>
-            <p>${movie.director}</p>
-        `);
-        movieTileContainer.append(createTile);
-    }
-};
 
 // FUNCTIONS TO TEST ONCLICK ATTRIBUTE OF BUTTON
 const testButton = () => {
@@ -50,61 +23,5 @@ function getEdit() {
         //console.log(button)
     }}
 
-//EDIT MOVIE FUNCTION - SENDS PATCH REQUEST
-async function editRequest(id) {
-    try {
-        const director = document.querySelector("#editDirector").value;
-        const title = document.querySelector('#editMovieTitle').value;
-        const rating = document.querySelector('#editRating').value;
-        const res = axios.post(urlGlitch/${movie.id}, {
-            title: `${title}`,
-            director: `${director}`,
-            rating: `${rating}`
-        });
-        await axios.patch(`${urlGlitch}/${id}`)
-        await timeout(1200)
-        console.log('WINNING')
-        allMoviesAdded()
-    } catch (e) {
-        console.log(`delete request failed, ${e}`)
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function fartsSmell (num) {
-    // code I want to execute
-    let totalStink = num * 1.75;
-    return totalStink
-}
-
-fartsSmell(3);
 
 
